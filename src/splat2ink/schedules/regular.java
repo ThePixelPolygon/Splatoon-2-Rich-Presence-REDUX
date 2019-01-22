cfd18c8e3282dac6b6e1f6a902becb6430cc8e99
thepixelpolygon <ThePixelPolygon@users.noreply.github.com>
2019-01-21 19:37:35 -0500

    Placed JSON data in the right places.

--- src/splat2ink/schedules/regular.java
+++ src/splat2ink/schedules/regular.java
@@ -1,5 +1,15 @@
 package splat2ink.schedules;
 
-public class regular {
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
+public class regular {
+    @SerializedName("stage_a")
+    @Expose
+    public List<regStageA> regStageA;
+    @SerializedName("stage_b")
+    @Expose
+    public List<regStageB> regStageB;
 }
--- src/splat2ink/schedules/rootObject.java
+++ src/splat2ink/schedules/rootObject.java
@@ -5,11 +5,5 @@ import java.util.List;
 
 public class rootObject {
     public long id;
-
-    @SerializedName("stage_a")
-    @Expose
-    public List<regStageA> regStageA;
-    @SerializedName("stage_b")
-    @Expose
-    public List<regStageB> regStageB;
+    public regular regular;
 }
